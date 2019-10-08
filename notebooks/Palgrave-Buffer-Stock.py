1846f0fa520e3f369bce03ab879f9100f5a5ad2a
MateoVG <mateovegi@gmail.com>
2019-10-08 11:19:53 -0400

    Import default dictionary

--- notebooks/Palgrave-Buffer-Stock.py
+++ notebooks/Palgrave-Buffer-Stock.py
@@ -16,8 +16,7 @@
 # %%
 # Setup
 from HARK.ConsumptionSaving.ConsIndShockModel import IndShockConsumerType
-from HARK.utilities import plotFuncsDer, plotFuncs
-from time import clock
+
 import matplotlib.pyplot as plt
 import numpy as np
 mystr = lambda number : "{:.4f}".format(number)
@@ -30,50 +29,19 @@ from scipy.optimize import root_scalar
 # %% {"code_folding": []}
 # Create two consumers, a perfect foresight one and one with shocks to income
 
-# Standard parameters
-IdiosyncDict={
-    # Parameters shared with the perfect foresight model
-    "CRRA": 2.0,                           # Coefficient of relative risk aversion
-    "Rfree": 1.03,                         # Interest factor on assets
-    "DiscFac": 0.96,                       # Intertemporal discount factor
-    "LivPrb" : [0.98],                     # Survival probability
-    "PermGroFac" :[1.0],                  # Permanent income growth factor
-    
-    # Parameters that specify the income distribution over the lifecycle
-    "PermShkStd" : [0.0],                  # Standard deviation of log permanent shocks to income
-    "PermShkCount" : 7,                    # Number of points in discrete approximation to permanent income shocks
-    "TranShkStd" : [0.8],                  # Standard deviation of log transitory shocks to income
-    "TranShkCount" : 7,                    # Number of points in discrete approximation to transitory income shocks
-    "UnempPrb" : 0.0,                     # Probability of unemployment while working
-    "IncUnemp" : 0.0,                      # Unemployment benefits replacement rate
-    "UnempPrbRet" : 0.0,                # Probability of "unemployment" while retired
-    "IncUnempRet" : 0.0,                   # "Unemployment" benefits when retired
-    "T_retire" : 0,                        # Period of retirement (0 --> no retirement)
-    "tax_rate" : 0.0,                      # Flat income tax rate (legacy parameter, will be removed in future)
-    
-    # Parameters for constructing the "assets above minimum" grid
-    "aXtraMin" : 0.001,                    # Minimum end-of-period "assets above minimum" value
-    "aXtraMax" : 20,                       # Maximum end-of-period "assets above minimum" value
-    "aXtraCount" : 48,                     # Number of points in the base grid of "assets above minimum"
-    "aXtraNestFac" : 3,                    # Exponential nesting factor when constructing "assets above minimum" grid
-    "aXtraExtra" : [None],                 # Additional values to add to aXtraGrid
-    
-    # A few other paramaters
-    "BoroCnstArt" : -500,                  # Artificial borrowing constraint; imposed minimum level of end-of period assets
-    "vFuncBool" : True,                    # Whether to calculate the value function during solution   
-    "CubicBool" : False,                   # Preference shocks currently only compatible with linear cFunc
-    "T_cycle" : 1,                         # Number of periods in the cycle for this agent type        
-    
-    # Parameters only used in simulation
-    "AgentCount" : 10000,                  # Number of agents of this type
-    "T_sim" : 120,                         # Number of periods to simulate
-    "aNrmInitMean" : -6.0,                 # Mean of log initial assets
-    "aNrmInitStd"  : 1.0,                  # Standard deviation of log initial assets
-    "pLvlInitMean" : 0.0,                  # Mean of log initial permanent income
-    "pLvlInitStd"  : 0.0,                  # Standard deviation of log initial permanent income
-    "PermGroFacAgg" : 1.0,                 # Aggregate permanent income growth factor
-    "T_age" : None,                        # Age after which simulated agents are automatically killed
-}
+# Import standard parameters
+from HARK.ConsumptionSaving.ConsumerParameters import init_idiosyncratic_shocks
+IdiosyncDict = init_idiosyncratic_shocks
+
+# Make modifications to fit our problem:
+
+# No artificial borrowing constraint
+IdiosyncDict["BoroCnstArt"] = -500
+# No unemployment
+IdiosyncDict["UnempPrb"] = 0
+# Ask HARK to compute the value function
+IdiosyncDict["vFuncBool"] = True
+
 
 # Create a copy with uncertainty turned off
 PFDict = copy(IdiosyncDict)
@@ -183,7 +151,7 @@ m0_locus = lambda m: m - (m-1)/(IdiosyncDict["Rfree"]/
                          IdiosyncDict["PermGroFac"][0])
 
 # Define grid of market resources
-m_max = 50
+m_max = 100
 m_grid = np.linspace(IndShockConsumer.solution[0].mNrmMin, m_max, 500)
 
 # Main lines
